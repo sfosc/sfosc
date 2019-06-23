7cbc95712e2c0ff7a73a5290741ae4d6a56cc426
Robin IT <info@robin-it.com>
2019-06-23 23:14:10 +0200

    Include CI compatible logic in the deploy script.

--- deploy-preview.sh
+++ deploy-preview.sh
@@ -2,33 +2,54 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Check our dependencies.
+if [ -z "$(which hugo)" ]; then
+	echo "Hugo must be installed and available in \$PATH"
+	exit 1
+fi
+if [ -z "$(which git)" ]; then
+	echo "Git must be installed and available in \$PATH"
+	exit 1
+fi
+
 # Make sure we're in the project root.
-toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
+# Note: this $0 only works if we call the script, so skip it for now.
+# toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
+toplevel="$(git rev-parse --show-toplevel)"
 cd "${toplevel}"
+
+# Check we're not in a dirty state.
+dirty_files=$(git status --porcelain --ignore-submodules 2> /dev/null)
+if [ -z "$FORCE_UPDATE" ] && [ ! -z "dirty_files" ]; then
+  echo "Looks like you have uncommitted changes, aborting."
+  echo "$dirty_files"
+  exit 1
+fi
 
 # Force checkout the remote preview master branch (detached).
 git submodule update --init --remote --checkout preview || exit 1
 
-# Build the project with docker into the submodule.
-docker run --rm \
-	-v $(pwd):/src \
-	-v $(pwd)/preview:/target \
-	--user $(id -u):$(id -g) \
-	klakegg/hugo:0.55.6 \
-	--config config.preview.toml
+# Find source and target commit hash.
+SOURCE_SHA="$(git rev-parse --verify HEAD)"
+TARGET_SHA="$(cat preview/.HEAD)"
+echo "Source SHA = $SOURCE_SHA"
+echo "Target SHA = $TARGET_SHA"
 
-# Go To Preview folder
-cd ./preview
+# When the target has a SHA reference, and they're the same, skip.
+if [ -z "$FORCE_UPDATE" ] && [ ! -z "$TARGET_SHA" ] && [ "$SOURCE_SHA" == "$TARGET_SHA" ]; then
+  echo "Source and last built commit are the same. No need to update."
+else
+  # Build the project into the submodule.
+  hugo -s . -d ./preview --config config.preview.toml
 
-# Add changes to git.
-git add .
+  # Track the SHA we've built from.
+  echo "$(git rev-parse --verify HEAD)" > preview/.HEAD
 
-# Commit changes.
-msg="manually rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+  # Go To Preview folder
+  cd ./preview
+
+  # Push preview repo from the detached head.
+  git add --all
+  git commit -m "Rebuilding site `date`"
+  git push --dry-run origin HEAD:master
 fi
-git commit -m "$msg"
-
-# Push preview repo from the detached head.
-git push origin HEAD:master
