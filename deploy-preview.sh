2c39dce6f8c404dfc236762bbbd16720c7bf6245
Robin van Boven <497556+Beanow@users.noreply.github.com>
2019-06-23 21:04:04 +0200

    Add a preview banner (#98)
    * Show a clear preview banner.
    * Include a publish script for the preview version.
    
    #98 feedback:
    * Use env for script shebangs.
    * Track preview as a submodule and force absolute paths in the deploy script.
    * Match normal and preview deploy script.
    * No need to come back up.

--- deploy-preview.sh
+++ deploy-preview.sh
@@ -6,19 +6,19 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 cd "${toplevel}"
 
-# Force checkout the remote public master branch (detached).
-git submodule update --init --remote --checkout public || exit 1
+# Force checkout the remote preview master branch (detached).
+git submodule update --init --remote --checkout preview || exit 1
 
 # Build the project with docker into the submodule.
 docker run --rm \
 	-v $(pwd):/src \
-	-v $(pwd)/public:/target \
+	-v $(pwd)/preview:/target \
 	--user $(id -u):$(id -g) \
 	klakegg/hugo:0.55.6 \
-	--config config.toml
+	--config config.preview.toml
 
-# Go To Public folder
-cd public
+# Go To Preview folder
+cd ./preview
 
 # Add changes to git.
 git add .
@@ -30,5 +30,5 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push preview repo from the detached head.
 git push origin HEAD:master
