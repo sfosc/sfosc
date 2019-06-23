58e3861a5b8186a8256d3ea81c65ffb6b6d3d814
Robin IT <info@robin-it.com>
2019-06-23 20:27:55 +0200

    Match normal and preview deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,35 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Make sure we're in the project root.
+toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
+cd "${toplevel}"
+
+# Force checkout the remote public master branch (detached).
+git submodule update --init --remote --checkout public || exit 1
+
+# Build the project with docker into the submodule.
+docker run --rm \
+	-v $(pwd):/src \
+	-v $(pwd)/public:/target \
+	--user $(id -u):$(id -g) \
+	klakegg/hugo:0.55.6 \
+	--config config.toml
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="manually rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-
+git push origin HEAD:master
 # Come Back up to the Project Root
 cd ..
